2806caecd3465a49e3a53e8ab74795a592d18b1c
Artem Shchirov <artemshchirov@gmail.com>
2022-11-27 16:57:23 +0200

    feat: King, Knight, Pawn move by the rules

--- src/models/figures/Knight.ts
+++ src/models/figures/Knight.ts
@@ -7,10 +7,17 @@ import whiteLogo from '../../assets/knight-white.svg'
 
 
 export class Knight extends Figure {
-
   constructor(color: Colors, cell: Cell) {
     super(color, cell)
     this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
     this.name = FigureNames.KNIGHT;
+  }
+
+  canMove(target: Cell): boolean {
+    if (!super.canMove(target)) return false;
+    const dx = Math.abs(this.cell.x - target.x);
+    const dy = Math.abs(this.cell.y - target.y);
+    
+    return (dx === 1 && dy === 2) || (dx === 2 && dy === 1)
   }
 }
